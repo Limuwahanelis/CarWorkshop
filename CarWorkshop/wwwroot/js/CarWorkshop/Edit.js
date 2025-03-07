60c07fb2bedaefc5189bfd1539f788e0fb2f464d
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-07 19:24:43 +0100

    Fix creating service bug.

--- CarWorkshop/wwwroot/js/CarWorkshop/Edit.js
+++ CarWorkshop/wwwroot/js/CarWorkshop/Edit.js
@@ -1,10 +1,11 @@
-﻿$(document).ready(function () {
-    $("#createCarWorkshopServiceModal form").submit(function (event) {
+﻿$(function () {
+    $("#createCarWorkshopServiceModal form").on("submit",function (event) {
         event.preventDefault();
 
         $.ajax({
             url: $(this).attr('action'),
             type: $(this).attr('method'),
+            data: $(this).serialize(),
             success: function (data) {
                 toastr["success"]("Created carworkshop service")
             },
