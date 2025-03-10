4351b869ccbbc8ccbd307852456ae51270a2918d
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-10 12:51:52 +0100

    Add tests for CarWorkshop entity

---
+++ CarWorkshop.DomainTests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using Xunit.Sdk;
+
+namespace CarWorkshop.Domain.Entities.Tests
+{
+    public class CarWorkshopTests
+    {
+        [Fact()]
+        public void EncodeName_SetEncodedName()
+        {
+            CarWorkshop carWorkshop = new CarWorkshop();
+            carWorkshop.Name = "Test Workshop";
+
+            carWorkshop.EncodeName();
+
+            Xunit.Assert.Equal( "test-workshop", carWorkshop.EncodedName);
+        }
+        [Fact()]
+        public void EncodeName_NameIsNull_ThorwException()
+        {
+            CarWorkshop carWorkshop = new CarWorkshop();
+
+            Action action = () => carWorkshop.EncodeName(); 
+
+            Xunit.Assert.Throws<NullReferenceException>( action);
+        }
+    }
+}
